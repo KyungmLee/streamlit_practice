63c5508b73ca58d60e8d87792452ea6403a77118
KyungmiLee <41696228+KyungmLee@users.noreply.github.com>
2024-04-29 15:40:06 +0900

    Update map.py

--- utils/map.py
+++ utils/map.py
@@ -6,22 +6,22 @@ import folium
 from streamlit_folium import st_folium, folium_static
 
 
-@st.cache_data
-def load_geo_json(file_path):
-    with open(file_path, encoding='utf-8') as f:
-        geo_gg = json.loads(f.read())
-    return geo_gg
+# @st.cache_data
+# def load_geo_json(file_path):
+#     with open(file_path, encoding='utf-8') as f:
+#         geo_gg = json.loads(f.read())
+#     return geo_gg
 
-@st.cache_data
-def load_excel_data(file_path):
-    return pd.read_excel(file_path)
+# @st.cache_data
+# def load_excel_data(file_path):
+#     return pd.read_excel(file_path)
 
-def load_data(file_path):
-    fname, ext = os.path.splitext(file_path)
-    if ext in ['xlsx', 'xls']:
-        return pd.read_excel(file_path)
-    elif ext == 'csv':
-        return pd.read_csv(file_path, encodings=['utf-8','euc-kr'])
+# def load_data(file_path):
+#     fname, ext = os.path.splitext(file_path)
+#     if ext in ['xlsx', 'xls']:
+#         return pd.read_excel(file_path)
+#     elif ext == 'csv':
+#         return pd.read_csv(file_path, encodings=['utf-8','euc-kr'])
 
 def draw_map(year, geo, df):
     map = folium.Map(location=[37.5666, 126.9782], zoom_start=8)
